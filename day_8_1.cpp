d09002a9307bc518890568586e420afbac3f9d63
Luka Barbić <luka.barbic0@gmail.com>
2022-12-08 11:28:02 +0100

    get rid of magic numbers

--- day_8_1.cpp
+++ day_8_1.cpp
@@ -7,8 +7,11 @@ char bottom[100][100];
 char left[100][100];
 char right[100][100];
 
+#define WIDTH 99
+#define HEIGHT 99
+
 bool visible(int i, int j) {
-    if (i == 0 || i == 99 - 1 || j == 0 || j == 99 - 1) {
+    if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH - 1) {
         return true;
     }
 
@@ -21,38 +24,40 @@ bool visible(int i, int j) {
 }
 
 int main() {
-    for (int i = 0; i < 99; ++i) {
-        for (int j = 0; j < 99; ++j) {
+    for (int i = 0; i < HEIGHT; ++i) {
+        for (int j = 0; j < WIDTH; ++j) {
             std::cin >> mat[i][j];
         }
     }
 
-    for (int j = 0; j < 99; ++j) {
+    for (int j = 0; j < WIDTH; ++j) {
         top[0][j] = mat[0][j];
-        left[j][0] = mat[j][0];
-
-        bottom[99-1][j] = mat[99-1][j];
-        right[j][99-1] = mat[j][99-1];
+        bottom[HEIGHT-1][j] = mat[WIDTH-1][j];
     }
 
-    for (int i = 1; i < 99; ++i) {
-        for (int j = 0; j < 99; ++j) {
+    for (int i = 0; i < HEIGHT; ++i) {
+        left[i][0] = mat[i][0];
+        right[i][WIDTH-1] = mat[i][WIDTH-1];
+    }
+
+    for (int i = 1; i < HEIGHT; ++i) {
+        for (int j = 0; j < WIDTH; ++j) {
             top[i][j] = std::max(mat[i][j], top[i - 1][j]);
-            bottom[99 - i - 1][j] = std::max(mat[99 - i - 1][j], bottom[99 - i][j]);
+            bottom[HEIGHT - i - 1][j] = std::max(mat[HEIGHT - i - 1][j], bottom[HEIGHT - i][j]);
         }
     }
 
-    for (int j = 1; j < 99; ++j) {
-        for (int i = 0; i < 99; ++i) {
+    for (int j = 1; j < WIDTH; ++j) {
+        for (int i = 0; i < HEIGHT; ++i) {
             left[i][j] = std::max(mat[i][j], left[i][j - 1]);
-            right[i][99 - j - 1] = std::max(mat[i][99 - j - 1], right[i][99 - j]);
+            right[i][WIDTH - j - 1] = std::max(mat[i][WIDTH - j - 1], right[i][WIDTH - j]);
         }
     }
 
     int result = 0;
 
-    for (int i = 0; i < 99; ++i) {
-        for (int j = 0; j < 99; ++j) {
+    for (int i = 0; i < HEIGHT; ++i) {
+        for (int j = 0; j < WIDTH; ++j) {
             result += visible(i, j);
         }
     }
